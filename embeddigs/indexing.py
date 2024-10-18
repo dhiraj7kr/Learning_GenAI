59d902e8fc21d81c91e7935350826424442fb63c
DHIRAJ KUMAR <110246188+dhiraj7kr@users.noreply.github.com>
2024-10-18 19:27:23 +0530

    Update indexing.py

--- embeddigs/indexing.py
+++ embeddigs/indexing.py
@@ -3,8 +3,8 @@ import pandas as pd
 from openai import AzureOpenAI
 import json
  
-embedding_client = AzureOpenAI(azure_endpoint="https://openai-azuretesting.openai.azure.com/",
-                               api_key="d57bdf8c8fd54d98aea4f20e0a183479",
+embedding_client = AzureOpenAI(azure_endpoint="<endpiont>",
+                               api_key="<api-key>",
                                api_version="2024-02-01")
  
 def extract_text_from_pdf(pdf_path):
